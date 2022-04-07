714db9d62e6fbe94a89de7c2e7c9cac2b920ba37
AlexWesleyy <allewss11@gmail.com>
2022-04-07 18:46:41 -0300

    atualizando

--- Questoes_Trabalhadas/Uri_1149.py
+++ Questoes_Trabalhadas/Uri_1149.py
@@ -1,11 +1,10 @@
-soma=0
-c=0
-while c<1:
-    a,n=input().split()
-    a=int(a)
-    n=int(n)
-    if n>0:
-        c+=1
+from posixpath import split
+
+
+a,n=input().split()
+a=int(a)
+n=int(n)
+soma=a
 for i in range(0,n):
-    soma=a+i
-print(soma+a)
+    soma+=i
+print(soma+(soma-1))
